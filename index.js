cfcae3c74e53d91d5efc60e66d8e7da753e4e6d1
gabrielasilva1991 <gabriela.silva1991@outlook.com>
2021-04-09 22:31:28 -0300

    correções rota doc

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-app.use('/', routesApi);
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/', routesApi);
 
 // app.get('*', (req, res) => {
 //   res.status(200).send({ message: 'Esta é a API do Burger Queen' });
