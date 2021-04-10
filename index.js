ae81551d5e77799b574678be12af8a4c2713e846
gabrielasilva1991 <gabriela.silva1991@outlook.com>
2021-04-09 22:59:26 -0300

    correção

--- index.js
+++ index.js
@@ -7,18 +7,21 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const routesApi = require('./server/routes/index');
 
-const port = process.env.PORT || 3000;
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.get('/doc-api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', routesApi);
-
-app.get('*', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // app.get('*', (req, res) => {
 //   res.status(200).send({ message: 'Esta é a API do Burger Queen' });
 // });
 
-app.listen(port, () => {
+app.listen(port, (error) => {
+  if (error) {
+    console.log(`There was a problem: ${error}`);
+    return;
+  }
   console.log(`O aplicativo está sendo executado em http://localhost:${port}`);
 });
