c425982efa7380b41ff09ce565551f0f370f2962
gabrielasilva1991 <gabriela.silva1991@outlook.com>
2021-04-09 21:28:38 -0300

    correção caminho swagger

--- index.js
+++ index.js
@@ -8,16 +8,15 @@ const swaggerDocument = require('./swagger.json');
 const routesApi = require('./server/routes/index');
 
 const port = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(express.json());
 app.use('/', routesApi);
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('*', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('*', (req, res) => {
-  res.status(200).send({ message: 'Esta é a API do Burger Queen' });
-});
+// app.get('*', (req, res) => {
+//   res.status(200).send({ message: 'Esta é a API do Burger Queen' });
+// });
 
 app.listen(port, () => {
   console.log(`O aplicativo está sendo executado em http://localhost:${port}`);
